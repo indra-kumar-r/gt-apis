c2560caba23e09f6d0191e7770fc94a7f924c297
INDRA KUMAR R <110984003+indra-kumar-r@users.noreply.github.com>
2025-04-09 12:58:41 +0530

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,3 +1,11 @@
 export default function handler(req, res) {
-    res.status(200).json({ location: "New York", temperature: "15°C" });
+    const citiesData = [
+        { name: 'New York', temperature: 22, condition: 'Clear' },
+        { name: 'London', temperature: 16, condition: 'Cloudy' },
+        { name: 'Mumbai', temperature: 30, condition: 'Rainy' },
+        { name: 'Tokyo', temperature: 25, condition: 'Clear' },
+        { name: 'Paris', temperature: 18, condition: 'Cloudy' },
+        { name: 'Sydney', temperature: 20, condition: 'Rainy' },
+    ];
+    res.status(200).json(citiesData);
 }
